8b7a7a1ee42bb51306dacdb1cc4ec772a656029d
Santiago Salas <santiagosalas.6532@gmail.com>
2022-03-21 20:36:35 -0500

    Initial commit, Generate StatelessWidgets Description

---
+++ lib/review.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Review extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    final photo
+
+
+    return Row(
+      children: <Widget> [
+
+      ],
+    );
+  }
+
+
+}
